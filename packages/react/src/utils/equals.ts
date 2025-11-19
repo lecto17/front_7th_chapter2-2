4604f8d8e3d5342bd247ededc0b7ca4298c4e0cd
lecto17 <khwoo_rpsit@naver.com>
2025-11-19 11:05:03 +0900

    chore: eqauls 함수 구현

--- packages/react/src/utils/equals.ts
+++ packages/react/src/utils/equals.ts
@@ -3,9 +3,18 @@
  * 객체와 배열은 1단계 깊이까지만 비교합니다.
  */
 export const shallowEquals = (a: unknown, b: unknown): boolean => {
-  // 여기를 구현하세요.
-  // Object.is(), Array.isArray(), Object.keys() 등을 활용하여 1단계 깊이의 비교를 구현합니다.
-  return a === b;
+  if (Object.is(a, b)) return true;
+
+  if (typeof a === "object" && typeof b === "object") {
+    const targetA = Object.entries(a as Record<string, unknown>);
+    const targetB = Object.entries(b as Record<string, unknown>);
+
+    return (
+      targetA.length === targetB.length && targetA.every(([key, value]) => Object.is(value, b?.[key as keyof typeof b]))
+    );
+  }
+
+  return false;
 };
 
 /**
@@ -13,7 +22,17 @@ export const shallowEquals = (a: unknown, b: unknown): boolean => {
  * 객체와 배열의 모든 중첩된 속성을 재귀적으로 비교합니다.
  */
 export const deepEquals = (a: unknown, b: unknown): boolean => {
-  // 여기를 구현하세요.
-  // 재귀적으로 deepEquals를 호출하여 중첩된 구조를 비교해야 합니다.
-  return a === b;
+  if (Object.is(a, b)) return true;
+
+  if (typeof a === "object" && typeof b === "object") {
+    const targetA = Object.entries(a as Record<string, unknown>);
+    const targetB = Object.entries(b as Record<string, unknown>);
+
+    return (
+      targetA.length === targetB.length &&
+      targetA.every(([key, value]) => deepEquals(value, b?.[key as keyof typeof b]))
+    );
+  }
+
+  return false;
 };
