6f7873a16904086a1fa07656c30feda3af01372d
lecto17 <khwoo_rpsit@naver.com>
2025-11-20 17:02:56 +0900

    feat: 항상 최신 상태를 참조하면서도, 함수 자체의 참조는 변경되지 않는 콜백 구현

--- packages/react/src/hooks/useAutoCallback.ts
+++ packages/react/src/hooks/useAutoCallback.ts
@@ -9,7 +9,14 @@ import { useRef } from "./useRef";
  * @returns 참조가 안정적인 콜백 함수
  */
 export const useAutoCallback = <T extends AnyFunction>(fn: T): T => {
-  // 여기를 구현하세요.
-  // useRef와 useCallback을 조합하여 구현해야 합니다.
-  return fn;
+  // useRef를 사용하여 최신 함수를 저장합니다.
+  const fnRef = useRef<T>(fn);
+
+  // 매 렌더링마다 최신 함수로 업데이트합니다.
+  fnRef.current = fn;
+
+  // useCallback으로 안정적인 참조를 가진 래퍼 함수를 반환합니다.
+  // 이 래퍼 함수는 항상 fnRef.current를 호출하므로 최신 함수를 실행합니다.
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return useCallback(((...args: any[]) => fnRef.current(...args)) as T, []);
 };
